b6b2afe1ac034b6f48cd552252c4d35e0b1273cb
Satish Autade <sautade@aconex.com>
2015-08-05 11:40:47 +0530

    Enabled sauce tunnel to run against local aconex app added sauce-connect gem

--- cloud_config.rb
+++ cloud_config.rb
@@ -9,3 +9,10 @@ ENV['browser_version'] ||= '8'
 
 ENV['SAUCE_USERNAME'] = 'satishautade'
 ENV['SAUCE_ACCESS_KEY'] = 'b38d994a-40a5-4ac9-84de-1e23c4a26457'
+ENV['tunnel'] ||= ''
+
+if ENV['tunnel'].empty?
+  require 'sauce'
+  Sauce::Utilities::Connect.start
+  ENV['base_url'] = 'http://aconex-local:8443'
+end
